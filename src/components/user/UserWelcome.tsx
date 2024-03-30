a98ce1581c3dbb61e0eb8094fcb290767f0fda64
Ben Creasy <b.creasy62@gmail.com>
2024-03-30 15:37:56 -0400

    swagmoney

--- src/components/user/UserWelcome.tsx
+++ src/components/user/UserWelcome.tsx
@@ -6,5 +6,5 @@ import useSWR from "swr";
 export default function UserWelcome() {
   const { data: user } = useSWR("/user", getUser);
 
-  return <p>This is a client component to get the user: {user?.name}</p>;
+  return <p>This is a client component to get the user to be a cool guy:  {user?.name}</p>;
 }
